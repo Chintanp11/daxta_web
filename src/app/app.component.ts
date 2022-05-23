d0420b302f69107e986d86a84ee8f9960a7aab6d
Eww Backend <aarohi.pithadiya@excellentwebworld.in>
2022-05-23 16:06:41 +0530

    browser language issue fixed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   constructor(){
     var userLang = window.navigator.language;
     console.log(userLang);
-    if(userLang === 'es' || userLang === 'es-ES'){
+    if(userLang === 'es' || userLang === 'es-ES' || userLang === 'es-es' ){
       localStorage.setItem('language',JSON.stringify('es'));
     } else if(userLang == 'pt-BR'){
       localStorage.setItem('language', JSON.stringify('br'));
